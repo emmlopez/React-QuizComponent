c28696ab2de199ef8a36e5868bbc6f57a2ec5159
elopez6 <Emmanuel.Lopez@coxautoinc.com>
2018-07-01 18:36:15 -0400

    add quizEnd class

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,20 +1,30 @@
-import React, { Component } from 'react';
-import QuizQuestion from './QuizQuestion';
-let quizData = require('./quiz_data.json');
+import React, { Component } from "react";
+import QuizQuestion from "./QuizQuestion";
+import QuizEnd from "./QuizEnd";
+
+let quizData = require("./quiz_data.json");
 
 class Quiz extends Component {
   constructor(props) {
     super(props);
-    this.state = { quiz_position: 1 }
+    this.state = { quiz_position: 1 };
   }
 
   render() {
     const { quiz_position } = this.state;
+    const isQuizEnd =
+      this.state.quiz_position - 1 === quizData.quiz_questions.length;
     return (
       <div>
-        <QuizQuestion quiz_question={quizData.quiz_questions[quiz_position - 1]} />
+        {isQuizEnd ? (
+          <QuizEnd />
+        ) : (
+          <QuizQuestion
+            quiz_question={quizData.quiz_questions[quiz_position - 1]}
+          />
+        )}
       </div>
-    )
+    );
   }
 }
 export default Quiz;
